812e4caaabd86c38ebdb19182bca8a7cb259780e
Valerio <vale-bond@hotmail.it>
2022-09-14 16:13:46 +0200

    removed unused methods

--- src/test/java/com/sourcesense/assignment/ControllerTest.java
+++ src/test/java/com/sourcesense/assignment/ControllerTest.java
@@ -37,24 +37,6 @@ public abstract class ControllerTest {
         }
     }
 
-    protected <T, O> O performAndExpect(MockHttpServletRequestBuilder mockHttpServletRequestBuilder, T request, Class<O> response, ResultMatcher... resultMatchers) {
-        try {
-            MvcResult mvcResult = performAndExpect(mockHttpServletRequestBuilder.content(objectMapper.writeValueAsString(request)), resultMatchers);
-            return parse(response, mvcResult);
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
-    }
-
-    protected <T, O> O performAndExpect(MockHttpServletRequestBuilder mockHttpServletRequestBuilder, Class<O> response, ResultMatcher... resultMatchers) {
-        try {
-            MvcResult mvcResult = performAndExpect(mockHttpServletRequestBuilder, resultMatchers);
-            return parse(response, mvcResult);
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
-    }
-
     protected MvcResult performAndExpect(MockHttpServletRequestBuilder mockHttpServletRequestBuilder, ResultMatcher... resultMatchers) {
         try {
             ResultActions resultActions = mockMvc.perform(mockHttpServletRequestBuilder
@@ -69,11 +51,6 @@ public abstract class ControllerTest {
         } catch (Exception e) {
             throw new IllegalStateException(e);
         }
-    }
-
-    private <O> O parse(Class<O> response, MvcResult mvcResult) throws java.io.IOException {
-        String contentAsString = mvcResult.getResponse().getContentAsString();
-        return objectMapper.readValue(contentAsString, response);
     }
 
     private <O> O parse(Class<? extends Collection> collection, Class<?> response, MvcResult mvcResult) throws java.io.IOException {
